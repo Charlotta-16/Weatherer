e4a19b41cce40d8b5841f9448a6a4fdb6efcdde6
Charlotta16 <113078594+Charlotta16@users.noreply.github.com>
2023-11-02 15:36:16 -0600

    Initial Creation

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public void run() {
+        //TO-DO Write script
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+}
